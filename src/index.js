04a42046732a06966fe8a4492904012372781164
Simón A Azócar <saimn.azocar@gmail.com>
2022-06-11 14:25:59 -0400

    addToCart on nosotros

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const burger = document.querySelector("#burger");
-
 const navbar = document.querySelector(".navbar__ul");
-
 
 const openNavBar = () => {
   navbar.classList.toggle("navbar__mobile");
@@ -10,3 +8,44 @@ const openNavBar = () => {
 }
 
 burger.addEventListener("click", openNavBar);
+
+const modal = document.querySelector("#myModal");
+const openModal = document.querySelector('.addItem-button');
+const span = document.getElementsByClassName("close")[0];
+
+
+openModal.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+let cart = []
+
+const itemInput = document.querySelector("#itemInput");
+const addButton = document.querySelector("#AddButton");
+const h3 = document.querySelector(".subtitle");
+
+const addToCart = () => {
+  const value = itemInput.value;
+  cart.push(value);
+  itemInput.value = '';
+
+  const para = document.createElement("p");
+  const node = document.createTextNode(value);
+  para.appendChild(node);
+
+  h3.insertAdjacentElement("afterend", para);
+  console.log(cart)
+}
+
+addButton.addEventListener("click", addToCart);
